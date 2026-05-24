891d71a12b980db740dfd589f32bc2b40b6b5b74
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-24 12:09:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)  # Vercel busca específicamente esta variable 'app'
 
 @app.route('/')
 def index():
-    return render_template('index.html', total_e=0, total_enc=0)
+    # Si tu HTML dice {{ variable }}, aquí tienes que definirla:
+    return render_template('index.html', 
+        total_e=0, 
+        total_enc=0,
+        otra_variable_que_te_marque_error=0
+    )
 
 # ... (tus otras rutas aquí)
 
