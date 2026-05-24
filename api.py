abe9fdf94eb5b85f051e966e8f8359f230658fae
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-24 12:33:15 -0600

    Update api.py

--- api.py
+++ api.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Esta función ayudará a que no falle si falta una variable en el HTML
-@app.context_processor
-def inject_defaults():
-    return dict(total_e=0, total_enc=0, nombre_usuario='Invitado')
-
 @app.route('/')
 def index():
     return render_template('index.html')
