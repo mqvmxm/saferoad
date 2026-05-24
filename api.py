624ec6aae65ab82a4b62488c24ed03d00655ba6b
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-05-24 12:38:39 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/entrevista')
-def entrevista():
-    return render_template('entrevista.html')
-
-@app.route('/encuesta')
-def encuesta():
-    return render_template('encuesta.html')
-
-@app.route('/resultados')
-def resultados():
-    return render_template('resultados.html')
+    return "¡HOLA! EL SERVIDOR FUNCIONA."
